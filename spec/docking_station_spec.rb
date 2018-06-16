617c16b09a56f002bdbec7d61e37075844cf5c62
Jay Khan <jkhan992@gmail.com>
2018-06-16 22:44:34 +0100

    cleaned up some formatting, house-keeping

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -5,8 +5,7 @@ describe DockingStation do
   # this test is checking if the release_bike method exists
   it { is_expected.to respond_to :release_bike }
 
-  # this test is checking that you can only release a
-  # bike after the bike is docked
+  # this test is checking you can only release a bike after the bike is docked
   describe '#release_bike' do
     it 'release working bike' do
       bike = Bike.new
@@ -24,8 +23,7 @@ describe DockingStation do
   # this test is checking if the doc_bike method exists
   it { is_expected.to respond_to :dock_bike }
 
-  # this test is checking capacity , if the capacity exceeds
-  # 20, throw an error
+  # this test is checking capacity, if the capacity exceeds 20, throw an error
   describe '#dock_bike' do
     it 'raise an error if no capacity available' do
       20.times { subject.dock_bike(Bike.new) }
@@ -36,41 +34,10 @@ describe DockingStation do
   # this test is checking if I can parse the doc_bike method 1 argument
   it { is_expected.to respond_to(:dock_bike).with(1).argument }
 
-  # ***** TO DO *******
-  # add a test for attribute reader
-
   # this test is checking if the dock_capacity_full method exists
   it { is_expected.to respond_to :dock_capacity_full? }
 
   # this test is checking if the dock_capacity_empty method exists
   it { is_expected.to respond_to :dock_capacity_empty? }
-=begin
-  # this test is checking if the dock is full
-begin
-  describe '#dock_full?' do
-    it 'checks that the dock is full' do
-      station = DockingStation.new
-      bike = Bike.new
-      20.times {subject.dock_bike(bike)}
-      expect(subject.dock_bike(bike)).to eq 20
-
-    end
-  end
-
-
-
-
-
-  # this test is checking if the dock is full
-  describe '#dock_empty?' do
-    it 'checks that the dock is empty' do
-      station = DockingStation.new
-      bike = Bike.new
-      20.times {subject.dock_bike(bike)}
-      20.times {subject.release_bike}
-      expect(subject.release_bike).to eq 1
-    end
-  end
-=end
 
 end
