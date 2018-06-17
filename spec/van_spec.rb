464f8531f4e76f39470f6a666ebbd9c9c190e938
Jay Khan <jkhan992@gmail.com>
2018-06-17 23:36:59 +0100

    added a test for Van Class

--- spec/van_spec.rb
+++ spec/van_spec.rb
@@ -17,7 +17,7 @@ describe Van do
   end
 
   describe '#send_broken_bike_to_van' do
-    it 'send 10 broken bike to a van' do
+    it 'send 10 broken bikes to a van' do
       station = DockingStation.new
       bike = Bike.new
       van = Van.new
@@ -28,32 +28,34 @@ describe Van do
     end
   end
 
-    describe '#collect_fixed_bikes' do
-      it 'receive broken bike' do
-        station = DockingStation.new
-        bike = Bike.new
-        van = Van.new
-        van.initalize
-        garage = Garage.new
-        garage.initalize
-        #van.send_broken_bike(Bike.new)
-        garage.fix_broken_bike(van.send_broken_bike(bike))
-        van.collect_fixed_bikes(garage)
-        expect(garage.fix_broken_bike_array.count).to eq 1
-      end
-    end
+  it { is_expected.to respond_to :collect_fixed_bikes }
 
-    describe '#collect_fixed_bikes' do
-      it 'receive broken bike' do
-        station = DockingStation.new
-        bike = Bike.new
-        van = Van.new
-        van.initalize
-        garage = Garage.new
-        garage.initalize
-        9.times { van.send_broken_bike(Bike.new) }
-        garage.fix_broken_bike(van.send_broken_bike(bike))
-        expect(garage.fix_broken_bike_array.count).to eq 10
-      end
+  describe '#collect_fixed_bikes' do
+    it 'receive broken bike' do
+      station = DockingStation.new
+      bike = Bike.new
+      van = Van.new
+      van.initalize
+      garage = Garage.new
+      garage.initalize
+      #van.send_broken_bike(Bike.new)
+      garage.fix_broken_bike(van.send_broken_bike(bike))
+      van.collect_fixed_bikes(garage)
+      expect(garage.fix_broken_bike_array.count).to eq 1
     end
+  end
+
+  describe '#collect_fixed_bikes' do
+    it 'receive broken bike' do
+      station = DockingStation.new
+      bike = Bike.new
+      van = Van.new
+      van.initalize
+      garage = Garage.new
+      garage.initalize
+      9.times { van.send_broken_bike(Bike.new) }
+      garage.fix_broken_bike(van.send_broken_bike(bike))
+      expect(garage.fix_broken_bike_array.count).to eq 10
+    end
+  end
 end
